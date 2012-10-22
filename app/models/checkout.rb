e1138fe6331aea3df05c296690993dc5d92510f7
Brian Norberg <brian_norberg@ncsu.edu>
2012-10-22 16:54:41 -0300

    Update app/models/checkout.rb

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,3 +1,3 @@
 class Checkout < ActiveRecord::Base
-  attr_accessible :date, :duration, :end_time, :id, :item_id, :patron_college, :patron_status, :renewals, :start_time, :transaction_id
+  attr_accessible :date, :duration, :end_time, :id, :item_id, :patron_college, :patron_status, :renewals, :start_time, :transaction_id, :created_at, :updated_at
 end
