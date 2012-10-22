01f7683346e430d1cbe6fe8ba46fe68d66e09264
Brian Robert Norberg <bnorberg02@gmail.com>
2012-10-22 16:39:01 -0400

    changes to models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ActiveRecord::Base
-  attr_accessible :call_number, :id, :location, :name
+  attr_accessible :call_number, :id, :location, :name, :created_at, :updated_at
   has_many :checkouts
 end
